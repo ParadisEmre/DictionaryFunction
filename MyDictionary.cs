9de5ac8c7d694ac163ce4f3bbeccf7a8ad8e6e44
Emre Özçatal <78852470+ParadisEmre@users.noreply.github.com>
2021-02-12 22:15:27 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryFunction
+{
+    class MyDictionary<T, L>
+    {
+        T[] items1;
+        L[] items2;
+
+        public MyDictionary()
+        {
+            items1 = new T[0];
+
+            items2 = new L[0];
+
+        }
+
+        public bool Count { get; internal set; }
+
+        public void Add(T item1, L item2)
+        {
+
+            T[] temp1 = items1;
+            L[] temp2 = items2;
+
+            items1 = new T[items1.Length + 1];
+            items2 = new L[items2.Length + 1];
+
+
+
+
+            for (int i = 0; i < temp1.Length; i++)
+            { 
+                items1[i] = temp1[i];
+                items2[i] = temp2[i];
+            }
+
+            items1[items1.Length - 1] = item1;
+            items2[items2.Length - 1] = item2;
+
+        }
+        public void toList()
+        {
+            for (int i = 0; i < items1.Length; i++)
+            {
+                Console.WriteLine(items1[i] + " : " + items2[i]);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DictionaryFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>() ;
+
+            myDictionary.Add("Emre", 18);
+            myDictionary.Add("Yeşim", 24);
+            myDictionary.Add("Selim", 10);
+            myDictionary.Add("Onur", 39);
+            myDictionary.Add("Bilge", 28);
+
+            myDictionary.toList();
+        }
+    }
+}
